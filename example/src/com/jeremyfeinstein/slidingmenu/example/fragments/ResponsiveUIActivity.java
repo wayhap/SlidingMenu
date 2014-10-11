12f27913637304ddcecc63a50d02bdd1bcdf4c3f
guanwei <guanwei@yaoji.com>
2014-10-11 16:45:52 +0800

    ...

--- example/src/com/jeremyfeinstein/slidingmenu/example/fragments/ResponsiveUIActivity.java
+++ example/src/com/jeremyfeinstein/slidingmenu/example/fragments/ResponsiveUIActivity.java
@@ -1,10 +1,10 @@
 package com.jeremyfeinstein.slidingmenu.example.fragments;
 
 import android.app.AlertDialog;
+import android.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.support.v4.app.Fragment;
 import android.view.View;
 
 import com.actionbarsherlock.view.MenuItem;
